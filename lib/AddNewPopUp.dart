1c56326537d3e2d41e310ae78478939cb40efb0e
Laheem Ayub <tyou67906@gmail.com>
2025-05-12 20:49:11 +0500

    maintained

--- lib/AddNewPopUp.dart
+++ lib/AddNewPopUp.dart
@@ -9,9 +9,13 @@ class AddNewPopUp extends StatelessWidget {
     required this.popUp,
     required this.addNewTask,
   });
+
   bool isPopUp;
   Function popUp, addNewTask;
+
   final TextEditingController _controller = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
   String? isEmpty(String? value) {
     if (value == null || value.trim().isEmpty) {
       return "Task cannot be empty";
@@ -33,7 +37,7 @@ class AddNewPopUp extends StatelessWidget {
                 color: Color.fromRGBO(128, 128, 128, 0.5),
                 spreadRadius: 2,
                 blurRadius: 5,
-                offset: Offset(0, 3), // changes position of shadow
+                offset: Offset(0, 3),
               ),
             ],
           ),
@@ -44,56 +48,61 @@ class AddNewPopUp extends StatelessWidget {
             ),
             width: isPopUp ? 330 : 0,
             height: isPopUp ? 250 : 0,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    IconButton(
-                      onPressed: () {
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      IconButton(
+                        onPressed: () {
+                          popUp();
+                        },
+                        icon: Icon(Icons.close_rounded),
+                      ),
+                    ],
+                  ),
+                  Text(
+                    "Add New Task",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(22.0),
+                    child: TextFormField(
+                      controller: _controller,
+                      validator: isEmpty,
+                      cursorColor: Colors.black,
+                      style: TextStyle(color: Colors.black),
+                      decoration: InputDecoration(
+                        focusColor: Colors.black,
+                        hintText: "Task",
+                        contentPadding: EdgeInsets.symmetric(
+                          vertical: 10,
+                          horizontal: 12,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.black,
+                      foregroundColor: Colors.white,
+                    ),
+                    onPressed: () {
+                      if (_formKey.currentState!.validate()) {
+                        addNewTask(_controller.text.trim());
                         popUp();
-                      },
-                      icon: Icon(Icons.close_rounded),
-                    ),
-                  ],
-                ),
-                Text(
-                  "Add New Task",
-                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(22.0),
-                  child: TextFormField(
-                    controller: _controller,
-                    validator: isEmpty,
-                    cursorColor: Colors.black,
-                    style: TextStyle(color: Colors.black),
-                    decoration: InputDecoration(
-                      focusColor: Colors.black,
-                      hintText: "Task",
-                      contentPadding: EdgeInsets.symmetric(
-                        vertical: 10,
-                        horizontal: 12,
-                      ),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                    ),
+                      }
+                    },
+                    child: Text("Ok"),
                   ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                  ),
-                  onPressed: () {
-                    popUp();
-                    addNewTask(_controller.text.toString());
-                  },
-                  child: Text("Ok"),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
